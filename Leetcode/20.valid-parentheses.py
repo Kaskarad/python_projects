74bf17ae75f8b268458c8046df1596e5bdd8d7fe
Dmitriy Kolesnikov <kolesnikov.ds@bk.ru>
2024-07-24 15:55:32 +0300

    20 solved

---
+++ Leetcode/20.valid-parentheses.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        my_open = "([{"
+        my_close = {')':'(',']':'[','}':'{'}
+        my_check = []
+        for i in s:
+            if i in my_open:
+                my_check.append(i)
+            elif len(my_check) > 0:
+                if my_close[i] is my_check[-1]:
+                    my_check.pop()
+                else:
+                    break
+            else:
+                my_check = [1]
+                break
+        if my_check == []:
+            return (True)
+        else:
+            return (False)
+        
+# @lc code=end
+
